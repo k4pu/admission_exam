ddcf59a835e7801ea177b42bad1cfe5b95ec6d74
y_ktn <sanponian@gmail.com>
2024-12-22 15:06:02 +0900

    changed db from sqlite3 to postgresql.

--- admission_exam_db/settings.py
+++ admission_exam_db/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+### added import modules
+from dotenv import load_dotenv
+load_dotenv()# .envファイル読み込み
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'admission_exam_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
