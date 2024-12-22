459ab7d60b47ba654e56328d0bf9139f2589e14b
y_ktn <sanponian@gmail.com>
2024-12-22 19:22:39 +0900

    added '__str__' method to Student class.

--- admission_exam_db/models.py
+++ admission_exam_db/models.py
@@ -4,3 +4,6 @@ class Student(models.Model):
     student_id = models.CharField(max_length=20, primary_key=True)
     family_name = models.CharField(max_length=20)
     given_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return " ".join([self.family_name, self.given_name])
