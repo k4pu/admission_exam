7c665740c8a628c325d7e3c0ad953735f62b8147
y_ktn <sanponian@gmail.com>
2024-12-23 07:09:05 +0900

    get student object at student_detail page.

--- admission_exam_db/views.py
+++ admission_exam_db/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from.models import Student
 
 def index(request):
     return HttpResponse("Hello this is admission exam db app!")
@@ -13,7 +15,9 @@ def students(request):
     return render(request, "admission_exam_db/students.html", context)
 
 def student_detail(request, student_id):
+    student = get_object_or_404(Student, student_id=student_id)
     context ={
         "student_id": student_id,
+        "student_name": " ".join([student.family_name, student.given_name])
     }
     return render(request, "admission_exam_db/student_detail.html", context)
