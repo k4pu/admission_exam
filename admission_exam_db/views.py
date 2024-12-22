768c53e4ecf4090336372c41035a7ac69a7a28a5
y_ktn <sanponian@gmail.com>
2024-12-22 17:36:27 +0900

    added login page.

--- admission_exam_db/views.py
+++ admission_exam_db/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello this is admission exam db app!")
+
+def login(request):
+    return HttpResponse("Hello this is login page!")
