76960959d05d884f158117a3cd948f64a75786a9
y_ktn <sanponian@gmail.com>
2024-12-22 19:09:09 +0900

    added student model.

--- admission_exam_db/models.py
+++ admission_exam_db/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=20, primary_key=True)
+    family_name = models.CharField(max_length=20)
+    given_name = models.CharField(max_length=20)
