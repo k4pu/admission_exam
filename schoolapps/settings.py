b95b9f3c46622a7c604e51f38633d549492c35ac
y_ktn <sanponian@gmail.com>
2025-01-08 21:03:34 +0900

    added static_dirs conf.

--- schoolapps/settings.py
+++ schoolapps/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # 静的ファイルのディレクトリ
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'admission_exam_db/static/',
+]  # 静的ファイルのディレクトリ
+STATIC_ROOT = '/app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
