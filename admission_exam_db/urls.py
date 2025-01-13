fe9883733903c8a17d0a9aa746d04999d77605f3
y_ktn <sanponian@gmail.com>
2025-01-13 11:34:38 +0900

    added login page.

--- admission_exam_db/urls.py
+++ admission_exam_db/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "admission_exam_db"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("student/", views.student, name="student"),
     path("student/<int:student_id>", views.student_detail, name="student_detail"),
     path("upload_university_faculty", views.upload_university_faculty, name="upload_university_faculty"),
--- schoolapps/settings.py
+++ schoolapps/settings.py
@@ -141,3 +141,5 @@ STATIC_ROOT = '/app/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admission_exam_db/'
